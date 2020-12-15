4cf1e3c6326cd5b0d2d16bd0067b1b55305bab8a
daniela <daniela.acuna93@gmail.com>
2020-12-15 14:40:58 -0600

    questions/functions

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
-// array of questions for user
-const questions = [
-    // TODO: Include packages needed for this application
-    
-    ];
-    // TODO: Create an array of questions for user input
-    const questions = [];
-    
-    // function to write README file
-    function writeToFile(fileName, data) {
+const inquirer = require('inquirer');
+const promptUser = () => {
+  return inquirer.prompt([
+
+    {
+      type: 'input',
+      name: 'Title',
+      message: 'What is the title for your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'input',
+      name: 'Usage',
+      message: 'What is this app used for?'
+    },
+    {
+      type: 'input',
+      name: 'Credits',
+      message: 'Please add contributors',
+    },
+    {
+      type: 'input',
+      name: 'Features',
+      message: 'If your projects has features,Add them here'
+     
+    },
+    {
+      type: 'input',
+      name: 'License',
+      message: 'What license was used for this README? ',
+    },
+    {
+      type: 'input',
+      name: 'Tests',
+      message: 'Lists for tests of application '
+     
+    },
+    {
+
+ 
     }
-    // TODO: Create a function to write README file
-    function writeToFile(fileName, data) {}
-    
-    // function to initialize program
-    function init() {
-    // TODO: Create a function to initialize app
-    function init() {}
-    
-    }
-    
-    // Function call to initialize app
-    init();
+  ]);
+};
+promptUser().then(answers => console.log(answers));
